88c38e49810719cd7ad48e5a296686da56826102
Badspler <4272723+Badspler@users.noreply.github.com>
2021-05-17 20:59:31 +1200

    Labels list-card div's so they act like big buttons for the checkbox

--- flair/static/js/setflair.js
+++ flair/static/js/setflair.js
@@ -1,23 +1,9 @@
-
-//alert("I am an alert box!");
-
-//var checkBox = document.querySelector('input[type="checkbox"]');
-//var textInput = document.querySelector('form-control').attr('required','required');
-//document.querySelector('.form-control').attr('required');
-
-//    alert(document.getElementById('trackerAccountName'));
-
 function setAccountTrackerRequired(){
     document.getElementById('trackerAccountName').required = true;
+    document.getElementById('trackerAccountName').disabled = false;
 }
 
 function setAccountTrackerNotRequired(){
     document.getElementById('trackerAccountName').required = false;
+    document.getElementById('trackerAccountName').disabled = true;
 }
-
-
-
-
-
-//<input class="form-control" type="text" id="trackerAccountName" name="trackerAccountName"
-//value="{{tracker_user_account_name}}" placeholder="e.g: https://myanimelist.net/animelist/[ThisUsername]" minlength="3" maxlength="20">
