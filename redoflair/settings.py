f5b3d272851317a187a8efec54c6a11df33eec6c
Erin <erin20913@gmail.com>
2021-05-30 14:33:52 -0400

    Add STATIC_URL to settings.py

--- redoflair/settings.py
+++ redoflair/settings.py
@@ -148,8 +148,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR 'static/
 STATIC_URL = '/static/'
-
 
 
 #setup global reddit, idk if this is the right place
