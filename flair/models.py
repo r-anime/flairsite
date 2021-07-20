0c8a80ea5893a5c797e3387923aa0ebb417b4ed8
Badspler <4272723+Badspler@users.noreply.github.com>
2021-07-20 21:54:47 +1200

    makes selection of flair-type conditioned on specific things

--- flair/models.py
+++ flair/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Q, UniqueConstraint
 from django.utils import timezone
 
 # DO stuff on logging aka trigger (signal)
@@ -12,7 +13,8 @@ class FlairType(models.Model):
     reddit_flair_emoji = models.CharField("Emoji that will be added if selected.", max_length=16)
     reddit_flair_text = models.CharField("Text that will be added if selected.", max_length=64, blank=True)
     reddit_flair_template_id = models.CharField("Reddit's Template ID to set if any", max_length=36, blank=True)
-    flair_type = models.CharField("Flairs Type", default="default", max_length=255)
+    FLAIR_TYPE_CHOICES = [('default', 'Default'), ('award', 'Award'), ('tiered-award', 'Tiered-Award'), ('temporary', 'Temporary')]
+    flair_type = models.CharField("Flairs Type", default="default", choices=FLAIR_TYPE_CHOICES, max_length=255)
     note = models.CharField("An optional note about this flair.", default="", max_length=255, blank=True)
     order = models.IntegerField()  # used to order emojis/flairs when multiple added
     wiki_display = models.BooleanField(default=False)
@@ -25,7 +27,7 @@ class FlairType(models.Model):
 
 
 class FlairsAwarded(models.Model):
-    flair_id = models.ForeignKey(FlairType, null=True, on_delete=models.SET_NULL)  # Links to what flair, or null if somehow deleted
+    flair_id = models.ForeignKey(FlairType, limit_choices_to=Q(flair_type='award') | Q(flair_type='tiered-award'), null=True, on_delete=models.SET_NULL)  # Links to what flair, or null if somehow deleted
     display_name = models.CharField("A reddit username", max_length=20)  # Reddit names can be max 20 characters
     date_added = models.DateTimeField(default=timezone.now, blank=True)
     note = models.CharField("An optional note on why this was awarded.", default="", max_length=255, blank=True)
