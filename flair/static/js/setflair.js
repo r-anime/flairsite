66209a839e1cf4d5f4da2a9744bdaf97f46cf280
Durinthal <durinthal@gmail.com>
2023-01-14 23:45:09 -0500

    Add search box for general flairs

--- flair/static/js/setflair.js
+++ flair/static/js/setflair.js
@@ -7,3 +7,33 @@ function setAccountTrackerNotRequired(){
     document.getElementById('trackerAccountName').required = false;
     document.getElementById('trackerAccountName').disabled = true;
 }
+
+
+let delayTimer;
+function searchGeneralFlairs(event) {
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function () {
+        const searchText = document.getElementById("search-box").value.toLowerCase();
+        if (searchText.length > 1) {
+            const flairList = document.querySelectorAll('.list-card-general');
+            for (let flairCard of flairList) {
+                const flairName = flairCard.dataset.flairName.toLowerCase();
+                if (flairName.indexOf(searchText) !== -1) {
+                    flairCard.style.display = 'flex';
+                } else if (flairName !== "none") { // Always display "None" option
+                    flairCard.style.display = 'none';
+                }
+            }
+        } else {
+            // Undo hiding everything from a search.
+            const flairList = document.querySelectorAll('.list-card-general');
+            for (let flairCard of flairList) {
+                flairCard.style.display = 'flex';
+            }
+        }
+    }, 300);
+}
+
+window.onload = function() {
+    document.getElementById("search-box").value = '';
+}
